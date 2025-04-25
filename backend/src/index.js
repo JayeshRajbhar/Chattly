a15a8516e26609bb41c486f34eb660c0c3d94a8f
JayeshRajbhar <jayeshrajbhar30@gmail.com>
2025-04-25 10:16:57 +0530

    Completed 3

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,16 +26,16 @@ app.get('/', (req, res) => {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: "http://localhost:5173",
   credentials: true,
 }));
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html", "dist", "index.html"));
   }
   );}
 
